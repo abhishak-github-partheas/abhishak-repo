6d1ca3fe69edfac62c4b905bb47e266571b82a25
Abhishak Kapoor <abhishak.kapoor@partheas.com>
2012-09-28 19:15:38 +0530

    adding a cout statement in the abc.txt

--- abc.cpp
+++ abc.cpp
@@ -7,6 +7,7 @@ int main()
   int a;
   cout<<"enter a\n";
   cin>>a;
+  cout<<"This is a seo_title branch\n";
   cout<<"The valure entered by you is= "<<a<<endl;
   cout<<"this is in the new_branch\n";
   return 0;
